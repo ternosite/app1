9c076a18dffdf9213ad22e2dcdcf92664f77c358
viktor <alycasar@gmail.com>
2024-01-17 20:59:56 +0200

    add about page

--- main/views.py
+++ main/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   context = {
-    'title': 'Home', 
-    'content': 'Главная страница магазина - HOME'
+    'title': 'Home - Главная', 
+    'content': 'Магазин мебели - HOME'
   }
   return render(request, 'main/index.html', context)
 
 def about(request):
-  return HttpResponse('About page')
+  context = {
+    'title': 'Home - О нас', 
+    'content': 'О нас',
+    'text_on_page': 'lorem*10'
+  }
+  return render(request, 'main/about.html', context)
